b979824f51bf7e45fa04326bc5e9dee826b431ff
Sterben <sergey.matushkin.98@mail.ru>
2025-09-23 14:35:45 +0300

    feat: add css modules for header and footer

--- my-app/src/widgets/LayoutHeader/Header.tsx
+++ my-app/src/widgets/LayoutHeader/Header.tsx
@@ -1,6 +1,8 @@
+import styles from "./Header.module.css";
+
 export const Header = () => {
     return (
-        <header>
+        <header className={styles.header}>
             <h1>PostFlow App</h1>
         </header>
     )
