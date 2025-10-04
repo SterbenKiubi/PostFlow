667c79d99e769685173c850498530efa0aa1ddbf
Sterben <sergey.matushkin.98@mail.ru>
2025-10-04 16:56:12 +0300

    feat: add collapsing/expanding comments

--- my-app/src/widgets/PostList/PostList.tsx
+++ my-app/src/widgets/PostList/PostList.tsx
@@ -4,15 +4,21 @@ import type { Post } from "../../entities/post/ui/PostCard";
 import { PostCard } from "../../entities/post/ui/PostCard";
 import styles from "./PostList.module.css";
 import { withLoading } from "../../shared/lib/hoc/withLoading";
+import { mockComments } from "../../shared/mocks/comments";
 
 export const PostListContent = () => {
     return (
         <div className={styles.postList}>
-            {mockPosts.map((post: Post) => (
-                <Fragment key={post.id}>
-                    <PostCard post={post} />
-                </Fragment>
-            ))}
+            {mockPosts.map((post: Post) => {
+
+                const postComments = mockComments.filter(comment => comment.postId === post.id);
+                
+                return (
+                    <Fragment key={post.id}>
+                        <PostCard post={post} comments={postComments} />
+                    </Fragment>
+                );
+            })}
         </div>
     )
 };
