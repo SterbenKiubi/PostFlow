cd9741c9aad7bec7a4ceac79e16705e6f297df86
Sterben <sergey.matushkin.98@mail.ru>
2025-10-15 18:14:32 +0300

    feat: wrapped app in the provider store

--- my-app/src/app/App.tsx
+++ my-app/src/app/App.tsx
@@ -1,14 +1,18 @@
 import { useTheme } from '../shared/lib/theme/useTheme';
 import '../App.css';
 import { Router } from './providers/router/Router';
+import { store } from './providers/store/store';
+import { Provider } from 'react-redux';
 
 function App() {
   const { isDark } = useTheme();
 
   return (
+    <Provider store={store}>
       <div className={`app ${isDark ? 'dark' : 'light'}`}>
         <Router />
       </div>
+    </Provider>
   )
 }
 
--- my-app/src/entities/post/ui/PostCard.tsx
+++ my-app/src/entities/post/ui/PostCard.tsx
@@ -58,6 +58,3 @@ export const PostCard = ( { post, comments }: PostCardProps ) => {
         </div>
     )
 };
-
-
-
