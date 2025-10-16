38b1750891e70bb88630edb762706bb31b8eba10
Sterben <sergey.matushkin.98@mail.ru>
2025-10-16 09:27:21 +0300

    feat: add RTK Query api postsApi, createEntityAdapter postSlice, geting postsQuery at postsPage

--- my-app/src/app/providers/store/store.ts
+++ my-app/src/app/providers/store/store.ts
@@ -1,5 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { postsApi } from "../../../entities/post/api/postsApi";
+import postReducer from "../../../entities/post/model/slice/postsSlice"
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        [postsApi.reducerPath]: postsApi.reducer,
+        posts: postReducer,
+    },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(
+      postsApi.middleware,
+    ),
 });
